7f4a6314c0ac199542e38123db2f6024ccbc5ca6
Gabriel Lucchesi <gabrielhubnerlucchesi@gmail.com>
2023-05-21 22:50:48 -0300

    Snake Game Files

---
+++ main.py
@@ -0,0 +1,51 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+import scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = scoreboard.Scoreboard()
+
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Food collision
+    if snake.head.distance(food) < 20:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.grow()
+
+    # Wall collision:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    # Tail collision:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
